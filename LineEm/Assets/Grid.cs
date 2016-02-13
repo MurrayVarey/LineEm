7cc742e3e98a39bff0ef86b4e60f31ad4f64b459
Murray Varey <Murray.Varey@gmail.com>
2016-02-13 10:50:57 +0000

    Initial Commit
    Initial commit of Unity files

---
+++ LineEm/Assets/Grid.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour 
+{
+
+	public GameObject _tilePrefab;
+
+	public int _gridWidth = 3;
+	public int _gridHeight = 3;
+
+	void Start () 
+	{
+		CreateTiles();	
+	}
+
+	void CreateTiles()
+	{
+		for(int x=0; x<_gridWidth; ++x)
+		{
+			for(int z=0; z<_gridHeight; ++z)
+			{
+				Vector3 tilePosition = new Vector3(CalculateTilePosition(x, _gridWidth), 0, CalculateTilePosition(z, _gridHeight));
+				Instantiate(_tilePrefab, tilePosition, transform.rotation);
+			}
+		}
+	}
+
+	private int CalculateTilePosition(int tile, int maxTiles)
+	{
+		// The middle tile should be at (0, 0).
+		// Offset by half (assumes max tile value is odd for now).
+		return tile-((maxTiles-1)/2);
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
---
+++ LineEm/Assets/GridTile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridTile : MonoBehaviour 
+{
+	public Material _idleMaterial;
+	public Material _activeMaterial;
+
+	private Renderer _renderer;
+
+	void Start () 
+	{
+		_renderer = GetComponent<Renderer> ();
+	}
+
+	void OnMouseEnter()
+	{
+		_renderer.material = _activeMaterial;
+	}
+
+	void OnMouseExit()
+	{
+		_renderer.material = _idleMaterial;
+	}
+}
