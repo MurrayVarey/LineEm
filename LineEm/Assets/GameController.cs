be19c38298168c1df2e53ad3a026a4daa939cfd1
Murray Varey <Murray.Varey@gmail.com>
2016-02-13 12:51:12 +0000

    Adds GridData class
    Adds a GridData class. This will keep track of the states of the tiles,
    and determine whether someone has won.

--- LineEm/Assets/GameController.cs
+++ LineEm/Assets/GameController.cs
@@ -7,10 +7,14 @@ public class GameController : MonoBehaviour {
 	public int _gridHeight = 3;
 	private Grid _grid;
 
+	private GridData _gridData;
+
 	// Use this for initialization
 	void Start () {
 		_grid = GameObject.Find("Grid").GetComponent<Grid>();
 		_grid.CreateTiles(_gridWidth, _gridHeight);
+
+		_gridData = new GridData(_gridWidth, _gridHeight);
 	}
 	
 	// Update is called once per frame
---
+++ LineEm/Assets/GridData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+public class GridData
+{
+
+	enum eTileState
+	{
+		empty,
+		nought,
+		cross
+	}
+
+	private int _width;
+	private int _height;
+
+	private eTileState[,] _tileStates;
+
+	public GridData (int width, int height)
+	{
+		_width = width;
+		_height = height;
+		_tileStates = new eTileState[width, height];
+	}
+}
+
+
