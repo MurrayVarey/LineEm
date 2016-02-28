1f72bdb765e9117ee51486b3436f27f7f4be59b2
Murray Varey <Murray.Varey@gmail.com>
2016-02-28 22:13:03 +0000

    Add Chalkboard Texture
    Adds a chalkboard texture ... albeit not a great one.
    
    setting the UVs was a bit funny, presumably because I'm using cubes.
    Maybe better to use planes.

--- LineEm/Assets/View/Grid.cs
+++ LineEm/Assets/View/Grid.cs
@@ -24,8 +24,9 @@ public class Grid : MonoBehaviour
 				Vector3 tilePosition = new Vector3(CalculateTilePosition(column, _height), 0, CalculateTilePosition(row, _width));
 				GameObject tile = Instantiate(_tilePrefab, tilePosition, transform.rotation) as GameObject;
 				TileDisplay tileDisplay = tile.GetComponent<TileDisplay>();
-				tileDisplay._column = column;
-				tileDisplay._row = row;
+				//tileDisplay._column = column;
+				//tileDisplay._row = row;
+				tileDisplay.SetCoordinates(column, row);
 			}
 		}
 	}
--- LineEm/Assets/View/TileDisplay.cs
+++ LineEm/Assets/View/TileDisplay.cs
@@ -23,7 +23,8 @@ public class TileDisplay : MonoBehaviour
 	void Start () 
 	{
 		_renderer = GetComponent<Renderer> ();
-		_renderer.material = _idleMaterial;
+		SetMaterial(_idleMaterial);
+
 
 		_stateText = _stateDisplay.GetComponent<TextMesh>();
 		_stateText.text = "";
@@ -31,7 +32,7 @@ public class TileDisplay : MonoBehaviour
 
 	void OnMouseOver()
 	{
-		_renderer.material = _activeMaterial;
+		SetMaterial(_activeMaterial);
 		if(Input.GetMouseButtonDown(0))
 		{
 			EventManager.OnTileClickedEvent(this);
@@ -40,11 +41,29 @@ public class TileDisplay : MonoBehaviour
 
 	void OnMouseExit()
 	{
-		_renderer.material = _idleMaterial;
+		SetMaterial(_idleMaterial);
 	}
 
 	public void UpdateDisplay(GridData.eTileState state)
 	{
 		_stateText.text = state == GridData.eTileState.nought ? "O" : "X" ;
+	}
+
+	public void SetCoordinates(int column, int row)
+	{
+		_column = column;
+		_row = row;
+		//float uOffset = (float)column/3;
+		//float yOffset = (float)row/3;
+		//_renderer.material.SetTextureOffset("_MainTex", new Vector2(uOffset, yOffset));
+	}
+
+	private void SetMaterial(Material material)
+	{
+		_renderer.material = material;
+		float uOffset = 0.67f-(float)_column/3;
+		float vOffset = 0.67f-(float)_row/3;
+		_renderer.material.SetTextureScale("_MainTex", new Vector2(0.333f,0.333f));
+		_renderer.material.SetTextureOffset("_MainTex", new Vector2(uOffset, vOffset));
 	}
 }
