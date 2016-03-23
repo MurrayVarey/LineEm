1abc061ffb59d1b91f702a3a379a93db17e64945
Murray Varey <Murray.Varey@gmail.com>
2016-03-23 21:22:55 +0000

    Adds Game Manager
    This will contain all the game options, such as one or two player.
    
    I think that "GameController" should be changed to describe the fact
    that it only controls Noughts and Crosses.

---
+++ LineEm/Assets/InitScene/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public enum eMove
+	{
+		PlayerOne,
+		PlayerTwo
+	}
+	private eMove _currentMove;
+
+	public enum eNextMoveOptions
+	{
+		Alternating,
+		Random
+	}
+
+	private eNextMoveOptions _nextMoveOption;
+
+	private int _playerCount;
+
+	private static GameManager _instance;
+
+	public static GameManager Instance()
+	{
+		if(_instance == null)
+		{
+			_instance = new GameObject().AddComponent<GameManager>();
+		}
+		return _instance;
+	}
+
+	void Awake()
+	{
+		_currentMove = eMove.PlayerOne;
+		_nextMoveOption = eNextMoveOptions.Alternating;
+		_playerCount = 2;
+	}
+
+	public eMove GetNextMove()
+	{
+		int nextMove;
+		if(_nextMoveOption == eNextMoveOptions.Alternating)
+		{
+			nextMove = ((int)_currentMove+1)%2;
+		}
+		else
+		{
+			nextMove = Random.Range(0,2);
+		}
+		return (eMove)nextMove;
+	}
+
+	public void SetPlayerCount(int playerCount)
+	{
+		_playerCount = playerCount;
+		print("Player Count: " + _playerCount);
+	}
+
+}
---
+++ LineEm/Assets/View/Menu Scripts/PlayerCountControl.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCountControl : MonoBehaviour {
+
+	public void SetPlayerCount(int playerCount)
+	{
+		GameManager manager = GameManager.Instance();
+		manager.SetPlayerCount(playerCount);
+	}
+	
+}
